5b269bc9ddff097fd88527e681dcc74f25cb3619
Andrew DeCrescenzo <drewmaru@gmail.com>
2018-02-16 20:26:10 -0500

    February 16, 2018 Update

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,13 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     // this function runs when the DOM is ready, i.e. when the document has been parsed
 
 
-	// "Return to Top" Button
-	document.getElementById("return_to_top_image").addEventListener("click", function(){
-		document.body.scrollTop = 0; // For Safari
-	    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-	});
-
-
 	// Side Menu height equals Custom Blog Div height
 
 	if (document.getElementById('custom_blog_div')) {
@@ -47,12 +40,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
 		var article = document.getElementById("content").querySelectorAll(".database_article_title, .database_article_subtitle");
 		var contents = document.getElementsByClassName("database_article_contents");
-/*
-		var header = document.createElement("a");
-		var header_text = document.createTextNode("Contents");
-		header.appendChild(header_text);
-		contents.appendChild(header);
-*/
 
 		for (i = 0; i < article.length; i++) {
 			if ( article[i].classList.contains("database_article_title")) {
@@ -60,12 +47,12 @@ document.addEventListener("DOMContentLoaded", function() {
 				var text = document.createTextNode(article[i].innerHTML.replace("&amp;", "&"));
 
 				var img = document.createElement("img");
-				img.src = "../images/List Bullet Image and Space.png";
+				img.src = "images/List Bullet Image and Space.png";
 				a.appendChild(img);
 				a.appendChild(text);
 				a.appendChild(document.createElement("br"));
 				a.classList.add("database_article_contents_title");
-				a.setAttribute("href", "#" + article[i].getAttribute("id") )
+				a.setAttribute("href", "#" + article[i].getAttribute("id") );
 				contents[0].appendChild(a);
 
 			} else if ( article[i].classList.contains("database_article_subtitle")) {
@@ -73,12 +60,12 @@ document.addEventListener("DOMContentLoaded", function() {
 				var text = document.createTextNode(article[i].innerHTML.replace("&amp;", "&"));
 
 				var img = document.createElement("img");
-				img.src = "../images/Sub List Bullet Image and Space.png";
+				img.src = "images/Sub List Bullet Image and Space.png";
 				a.appendChild(img);
 				a.appendChild(text);
 				a.appendChild(document.createElement("br"));
 				a.classList.add("database_article_contents_subtitle");
-				a.setAttribute("href", "#" + article[i].getAttribute("id") )
+				a.setAttribute("href", "#" + article[i].getAttribute("id") );
 				contents[0].appendChild(a);
 			}
 
@@ -87,55 +74,195 @@ document.addEventListener("DOMContentLoaded", function() {
 	}
 
 
-	// Custom Lightbox
+	// Automatically implement Fresco Lightbox to images
 
 	if (document.getElementsByClassName("database_article_image_left")) {
 
 		//Lightbox code for database_article_image_left
 
-			//On image mouse over, append a semi-transparent white div on top of the image.
+		var article = document.getElementById("content").querySelectorAll(".database_article_image_left");
 
-/*
-	Document.querySelector(div.classname img)
-*/
+		for (i = 0; i < article.length; i++) {
+			var a = document.createElement("a");
+			a.setAttribute("href", article[i].firstElementChild.src);
+			a.setAttribute("class", "fresco");
+			a.setAttribute('data-fresco-caption', article[i].getElementsByClassName("database_article_image_text")[0].innerHTML );
+			article[i].appendChild(a);
 
-			document.getElementsByClassName("database_article_image_left_lightbox").onmouseover = function() { mouseOver() };
-				function mouseOver() {
-
-					console.log("hi");
-				}
-
-
-
-
-			};
-			
-
-			//On image mouse out, remove the appended semi-transparent white div.
-
-			//On image click, append a semi-transparent black div that fills the screen
-
-			//Append a smaller white div to the center of the screen on top of the full screen black div
-
-			//Append an X to the top right of the centered white div that removes the black div and the white div (and all of its contents)
-
-			//Append the clicked image to the centered white div
-
-			//Append the text description of the image to the centered white div
-
-			//Clicking outside the white div removes all of these (black div, white div, X button, image, and text description)
-
+			a.appendChild(article[i].getElementsByTagName("img")[0] );
+			article[i].insertBefore(a, article[i].getElementsByClassName("database_article_image_text")[0] );
+		}
 	}
 
 	if (document.getElementsByClassName("database_article_image_near_infobox")) {
 
 		//Lightbox code for database_article_image_near_infobox
+
+		var article = document.getElementById("content").querySelectorAll(".database_article_image_near_infobox");
+
+		for (i = 0; i < article.length; i++) {
+			var a = document.createElement("a");
+			a.setAttribute("href", article[i].firstElementChild.src);
+			a.setAttribute("class", "fresco");
+			a.setAttribute('data-fresco-caption', article[i].getElementsByClassName("database_article_image_text")[0].innerHTML );
+			article[i].appendChild(a);
+
+			a.appendChild(article[i].getElementsByTagName("img")[0] );
+			article[i].insertBefore(a, article[i].getElementsByClassName("database_article_image_text")[0] );
+		}
 	}
 
 	if (document.getElementsByClassName("database_article_image_right")) {
 
 		//Lightbox code for database_article_image_right
+
+		var article = document.getElementById("content").querySelectorAll(".database_article_image_right");
+
+		for (i = 0; i < article.length; i++) {
+			var a = document.createElement("a");
+			a.setAttribute("href", article[i].firstElementChild.src);
+			a.setAttribute("class", "fresco");
+			a.setAttribute('data-fresco-caption', article[i].getElementsByClassName("database_article_image_text")[0].innerHTML );
+			article[i].appendChild(a);
+
+			a.appendChild(article[i].getElementsByTagName("img")[0] );
+			article[i].insertBefore(a, article[i].getElementsByClassName("database_article_image_text")[0] );
+		}
 	}
+
+	if (document.getElementsByClassName("database_article_profile")) {
+
+		//Lightbox code for database_article_profile
+
+		var article = document.getElementById("database_article_side_menu");
+
+		if (article === null) {
+			article === 0; // "return" seems to stop the rest of the Javascript code after this function, but "article === 0" does not 
+		} else {
+
+			var a = document.createElement("a");
+//			console.log(article.getElementsByTagName("img")[0].src);
+			a.setAttribute("href", article.getElementsByTagName("img")[0].src);
+			a.setAttribute("class", "fresco");
+			a.setAttribute('data-fresco-caption', article.getElementsByTagName("img")[0].alt );
+			article.appendChild(a);
+
+			a.appendChild(article.getElementsByTagName("img")[0] );
+			article.insertBefore(a, article.firstChild );
+		}
+	}
+
+
+	// Header
+	
+		//Place header image into "header_and_navigation" div
+
+		var top = document.getElementById("header_and navigation");
+		var headerImg = document.createElement("img");
+
+		var newestRelease = document.createElement("a");
+		var newestReleaseImg = document.createElement("img");
+
+		headerImg.src = "images/Spirit Legends Main Banner v4.png";
+		headerImg.setAttribute("width", "960px");
+		headerImg.setAttribute("id", "header_image");
+		top.appendChild(headerImg);
+
+		newestReleaseImg.src = "images/Newest Release 1 v4.png"
+		newestRelease.setAttribute("id", "newest_release_button");
+		newestRelease.setAttribute("href", "#");
+		newestRelease.appendChild(newestReleaseImg);
+		top.appendChild(newestRelease);
+
+
+
+
+
+		//Create Navigation Bar
+
+		var navUnorderedList = document.createElement("ul");
+		navUnorderedList.setAttribute("id", "navigation");
+		top.appendChild(navUnorderedList);
+
+		var navHomeButton = document.createElement("li");
+		var navHomeButtonImg = document.createElement("img");
+		var navHomeButtonLink = document.createElement("a");
+		navHomeButton.setAttribute("id", "home_button");
+		navHomeButtonImg.src = "images/Button - Home v2.png";
+		navHomeButtonImg.setAttribute("width", "99px");
+		navHomeButtonImg.setAttribute("alt", "Home");
+		navHomeButtonImg.setAttribute("onmouseover", "src='images/Button - Home v2 - Hover.png'");
+		navHomeButtonImg.setAttribute("onmouseout", "src='images/Button - Home v2.png'");
+		navHomeButtonLink.setAttribute("href", "index.html");
+		navHomeButton.appendChild(navHomeButtonImg);
+		navHomeButtonLink.appendChild(navHomeButton);
+		navUnorderedList.appendChild(navHomeButtonLink);
+
+		var navSeriesButton = document.createElement("li");
+		var navSeriesButtonImg = document.createElement("img");
+		var navSeriesButtonLink = document.createElement("a");
+		navSeriesButton.setAttribute("id", "series_button");
+		navSeriesButtonImg.src = "images/Button - Series v2.png";
+		navSeriesButtonImg.setAttribute("width", "99px");
+		navSeriesButtonImg.setAttribute("alt", "Home");
+		navSeriesButtonImg.setAttribute("onmouseover", "src='images/Button - Series v2 - Hover.png'");
+		navSeriesButtonImg.setAttribute("onmouseout", "src='images/Button - Series v2.png'");
+		navSeriesButtonLink.setAttribute("href", "series.html");
+		navSeriesButton.appendChild(navSeriesButtonImg);
+		navSeriesButtonLink.appendChild(navSeriesButton);
+		navUnorderedList.appendChild(navSeriesButtonLink);
+
+		var navDatabaseButton = document.createElement("li");
+		var navDatabaseButtonImg = document.createElement("img");
+		var navDatabaseButtonLink = document.createElement("a");
+		navDatabaseButton.setAttribute("id", "series_button");
+		navDatabaseButtonImg.src = "images/Button - Database v2.png";
+		navDatabaseButtonImg.setAttribute("width", "99px");
+		navDatabaseButtonImg.setAttribute("alt", "Home");
+		navDatabaseButtonImg.setAttribute("onmouseover", "src='images/Button - Database v2 - Hover.png'");
+		navDatabaseButtonImg.setAttribute("onmouseout", "src='images/Button - Database v2.png'");
+		navDatabaseButtonLink.setAttribute("href", "database.html");
+		navDatabaseButton.appendChild(navDatabaseButtonImg);
+		navDatabaseButtonLink.appendChild(navDatabaseButton);
+		navUnorderedList.appendChild(navDatabaseButtonLink);
+
+
+	// Footer
+
+		//Place footer image into "footer" div
+
+		var bottom = document.getElementById("footer_div");
+		var footerImg = document.createElement("img");
+		footerImg.src = "images/Banner - Return to Top v2.jpg";
+		footerImg.setAttribute("id", "return_to_top_image");
+		footerImg.setAttribute("onmouseover", "src='images/Banner - Return to Top v2 - Hover.jpg'");
+		footerImg.setAttribute("onmouseout", "src='images/Banner - Return to Top v2.jpg'");
+		bottom.appendChild(footerImg);
+
+
+		//Bottom copyrights
+		
+		var bottomText = document.createElement("span");
+		var bottomTextImg = document.createElement("img");
+		var bottomLinkAndrew = document.createElement("a");
+		var bottomLinkSteven = document.createElement("a");
+
+		bottomText.setAttribute("class", "footer_text");
+		bottomText.innerHTML = 'Spirit Legends is copyright&copy; of <a href="http://andrewartwork.com" class= "footer_link">Andrew DeCrescenzo</a>. Website design is based on the now defunct website, Mega Man Monthly, which was designed by <a href="https://twitter.com/stevenchase87" class= "footer_link">Steven Chase</a>. All rights reserved.'
+
+		bottomTextImg.src = "images/Footer Image.jpg";
+		bottomTextImg.setAttribute("class", "footer_image");
+
+		bottom.appendChild(bottomText);
+		bottom.appendChild(bottomTextImg);
+
+
+	// "Return to Top" Button
+	document.getElementById("return_to_top_image").addEventListener("click", function(){
+		document.body.scrollTop = 0; // For Safari
+	    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+	});
+
 
 
 
